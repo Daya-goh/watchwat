e596afa48fe8273615dbe255b10662bacad2e57b
jgqy93 <jgqy93@gmail.com>
2022-08-23 10:12:28 +0800

    initial test passed/

--- src/Test/Fetch.jsx
+++ src/Test/Fetch.jsx
@@ -1,21 +1,43 @@
+import { useState } from "react";
+
 const Fetch = () => {
   const codeArray = [];
   const networkUrl =
     "https://api.watchmode.com/v1/list-titles/?apiKey=Xi2NW1MlhJARxSqBIbyUJD68ZjljDinK2iKSzG6F&network_ids=511";
 
-  const array = fetch(networkUrl)
+  fetch(networkUrl)
     .then((response) => response.json())
     .then((data) => {
       for (const title of data.titles) {
         codeArray.push(title.tmdb_id);
       }
     });
-  //   console.log(data.titles[0].tmdb_id);
-  console.log("-------------------");
-  console.log(codeArray);
+  /* -------------------------------------------------- */
+  const titleUrl =
+    "https://api.watchmode.com/v1/title/3186566/details/?apiKey=Xi2NW1MlhJARxSqBIbyUJD68ZjljDinK2iKSzG6F&title_id=3186566";
+
+  const titleImg = fetch(titleUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+  /* ------------------------------------------------------ */
+  const [show, setShow] = useState({});
+
+  const handleClick = () => {
+    fetch(titleUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setShow(data);
+      });
+  };
+
   return (
     <div>
       <h1>test</h1>
+      <h1>{show.title}</h1>
+      <img src={titleImg} alt="extraordinary woo" />
+      <button onClick={handleClick}> click</button>
     </div>
   );
 };
