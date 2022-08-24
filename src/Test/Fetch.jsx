ed56dca2101d1ae671a0427e0d46e2ae915f484f
jgqy93 <jgqy93@gmail.com>
2022-08-24 13:55:26 +0800

    fetching data complete

--- src/Test/Fetch.jsx
+++ src/Test/Fetch.jsx
@@ -1,45 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Fetch = () => {
-  const codeArray = [];
+  const [show, setShow] = useState([]);
+
+  let codeArray = [];
   const networkUrl =
-    "https://api.watchmode.com/v1/list-titles/?apiKey=Xi2NW1MlhJARxSqBIbyUJD68ZjljDinK2iKSzG6F&network_ids=511";
+    "https://api.watchmode.com/v1/list-titles/?apiKey=kNWi3tZZezuOie6laejFBpvlyeFcEcTtlxr9WRND&network_ids=511";
 
-  fetch(networkUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      for (const title of data.titles) {
-        codeArray.push(title.tmdb_id);
-      }
-    });
-  /* -------------------------------------------------- */
-  const [image, setImage] = useState({});
-  const titleUrl =
-    "https://api.watchmode.com/v1/title/3186566/details/?apiKey=Xi2NW1MlhJARxSqBIbyUJD68ZjljDinK2iKSzG6F&title_id=3186566";
+  useEffect(() => {
+    const fetchID = async () => {
+      codeArray = await fetch(networkUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          const showCodes = data?.titles?.map((id) => id.imdb_id);
+          const show2 = showCodes.slice(0, 2);
+          return show2;
+        });
+      console.log(codeArray);
 
-  fetch(titleUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      setImage(data.poster);
-    });
+      const array = await Promise.all(
+        codeArray.map((code) =>
+          fetch(
+            `https://api.watchmode.com/v1/title/${code}/details/?apiKey=kNWi3tZZezuOie6laejFBpvlyeFcEcTtlxr9WRND`
+          )
+            .then((response) => response.json())
+            .then((data) => data)
+        )
+      );
+      console.log(array);
+      setShow([...show, ...array]);
+    };
+    fetchID();
+  }, []);
 
   /* ------------------------------------------------------ */
-  const [show, setShow] = useState({});
-
-  const handleClick = () => {
-    fetch(titleUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        setShow(data);
-      });
-  };
 
   return (
     <div>
-      <h1>test</h1>
-      <h1>{show.title}</h1>
-      <img src={image} alt="extraordinary woo" />
-      <button onClick={handleClick}> click</button>
+      <h1>Testing</h1>
+      {show.map((detail) => (
+        <>
+          <h1>{detail.title}</h1>
+          <img src={detail.poster} alt="poster" />
+        </>
+      ))}
     </div>
   );
 };
