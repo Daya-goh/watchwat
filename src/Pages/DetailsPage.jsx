52d30780ee8557d7fe8a6c25124851754215e13a
jgqy93 <jgqy93@gmail.com>
2022-08-26 10:18:31 +0800

    show details video link embed done

--- src/Pages/DetailsPage.jsx
+++ src/Pages/DetailsPage.jsx
@@ -22,32 +22,23 @@ const DetailsPage = () => {
   return (
     <div className="flex flex-row">
       <div>
-        <div class="card w-96 bg-base-100 shadow-xl">
-          <figure class="px-10 pt-10">
-            <img src={showDetails.backdrop} />
-            {/* <div>
-              <iframe
-                src={showDetails.trailer_thumbnail}
-                frameborder="0"
-                width="335"
-                height="200"
-                allow="autoplay; encrypted-media"
-                allowfullscreen
-                title="video"
-              />{" "}
-            </div> */}
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure className="px-10 pt-10">
+            <a href={showDetails.trailer}>
+              <img src={showDetails.backdrop} />
+            </a>
           </figure>
-          <div class="card-body items-center text-center">
-            <h2 class="card-title text-2xl">{showDetails.title}</h2>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title text-2xl">{showDetails.title}</h2>
             <p>Rating: {showDetails.user_rating}</p>
             <p>{showDetails.genre_names}</p>
             <p>
               Release date:{" "}
               {Moment(showDetails.release_date).format("DD-MM-YYYY")}
             </p>
-            <div class="card-actions">
-              <button class="btn btn-primary">+</button>
-              <button class="btn btn-primary">♥️</button>
+            <div className="card-actions">
+              <button className="btn btn-primary">+</button>
+              <button className="btn btn-primary">♥️</button>
             </div>
           </div>
         </div>
---
+++ src/Pages/extraCode.js
@@ -0,0 +1,31 @@
+{
+  /* {showDetails.length>0 ? (
+    <div>{showDetails.title} loading...</div>
+  ) : (
+    <div>
+      <iframe
+        src={showDetails.trailer}
+        frameBorder="0"
+        width="335"
+        height="200"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+        title="video"
+      />{" "}
+    </div>
+  )} */
+}
+
+{
+  /* <div>
+    <iframe
+      src="https://www.youtube.com/watch?v=Eqmi5YPnrTM"
+      frameborder="0"
+      width="335"
+      height="200"
+      allow="autoplay; encrypted-media"
+      allowfullscreen
+      title="video"
+    />{" "}
+  </div> */
+}
