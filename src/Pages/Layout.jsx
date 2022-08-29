c3b6d42dd5e192f94c04be67265949fc57e76f8a
jgqy93 <jgqy93@gmail.com>
2022-08-29 17:30:12 +0800

    on enter done

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -14,7 +14,7 @@ const Layout = ({ addShow }) => {
         </div>
       </div>
       <div className="drawer-side">
-        <label for="my-drawer-4" className="drawer-overlay"></label>
+        <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
         <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
           <WatchList addShow={addShow} />
         </ul>
